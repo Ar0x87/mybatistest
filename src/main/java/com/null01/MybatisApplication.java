ee7d42a43aafc91c40bda8d03151899a81c212cc
Ar0x87 <roks88869@gmail.com>
2022-06-09 11:26:14 +0300

    Initial

--- src/main/java/com/null01/MybatisApplication.java
+++ src/main/java/com/null01/MybatisApplication.java
@@ -4,13 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.null01")
+@MapperScan("com.null01.mappers")
 @SpringBootApplication
 
-public class MybatistestApplication {
+public class MybatisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MybatistestApplication.class, args);
+        SpringApplication.run(MybatisApplication.class, args);
     }
 
 }
