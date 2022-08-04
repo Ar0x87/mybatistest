87eb37c8bb482348d2c14c4347a8ae1b8106a367
Ar0x87 <roks88869@gmail.com>
2022-08-04 08:49:06 +0300

    reqVal

--- src/main/java/com/null01/validators/annotations/FieldsValidation.java
+++ src/main/java/com/null01/validators/annotations/FieldsValidation.java
@@ -17,7 +17,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Documented
 public @interface FieldsValidation {
 
-    String message() default "Empty request";
+    String message() default "Empty hotelname and address";
 
     Class<?>[] groups() default { };
 
